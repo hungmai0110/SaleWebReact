a3b641b31780a8dbdd3546e301902f76fd3c6642
Hung <maingochungnuce@gmail.com>
2022-11-15 14:17:47 +0700

    update

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,19 +1,24 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:3001',
-    headers: { 'Content-type': 'application/json; charset=UTF-8' }
+  baseURL:
+    process.env.REACT_APP_BASE_URL ||
+    "https://leather-goods-website.herokuapp.com",
+  headers: { "Content-type": "application/json; charset=UTF-8" },
 });
 
 // Add a response interceptor
-axiosClient.interceptors.response.use(function (response) {
+axiosClient.interceptors.response.use(
+  function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response.data;
-  }, function (error) {
+  },
+  function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return Promise.reject(error);
-  });
+  }
+);
 
 export default axiosClient;
